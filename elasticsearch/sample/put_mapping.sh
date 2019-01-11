04f34593a49b477d3362376266ce93f5a8a6c808
k-kurumi <optpia.kurumi@gmail.com>
2019-01-11 13:07:43 +0900

    update mapping:

--- elasticsearch/sample/put_mapping.sh
+++ elasticsearch/sample/put_mapping.sh
@@ -2,9 +2,15 @@
 
 # 確認用マッピング
 
+# 削除(データも消える)
+curl -s -X DELETE 'http://localhost:9200/myindex2'
+
 # keyword: 完全一致でのみ検索可能で、グラフに使える(useragent割合をグラフ化するなど)
 # text: アナライザーで分かち書きなど検索対象になる(グラフに使えない)
-
+#
+# shardsはあとから変更できない
+# replicasはあとで変更できる
+# shard: 5, replica: 1 のとき、shardは全部で10個できる(shard5とレプリカ分5)
 curl -s -XPUT 'http://localhost:9200/myindex2' \
   -H 'Content-Type: application/json' \
   -d '
@@ -24,6 +30,12 @@ curl -s -XPUT 'http://localhost:9200/myindex2' \
         }
       }
     }
+  },
+  "settings": {
+    "index": {
+     "number_of_shards": 2,
+     "number_of_replicas": 1
+   }
   }
 }
 ' | jq .
